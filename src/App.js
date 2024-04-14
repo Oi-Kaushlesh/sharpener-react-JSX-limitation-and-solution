1f59d696d67f513b128b7009821b8e3e7abae27c
Oi-Kaushlesh <kaushlesh2611singh@gmail.com>
2024-04-14 19:35:20 +0530

    Refs and Controlled Components

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge , ucollegename) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, collegename : ucollegename, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </>
+  );
+}
+
+export default App;
